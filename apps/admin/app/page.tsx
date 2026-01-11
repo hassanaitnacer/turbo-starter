b79406789985bf302e853a65fe76743652bd375e
Hassan Ait Nacer <108420918+hassanaitnacer@users.noreply.github.com>
2026-01-11 11:12:03 +0100

    style(admin): adjust spacing between heading and paragraph in admin home page to match web

--- apps/admin/app/page.tsx
+++ apps/admin/app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
-    <main className="h-dvh flex flex-col gap-2 items-center justify-center">
-      <h1 className="text-3xl font-semibold">Nacer Codes</h1>
-      <p className="text-gray-10">This is the admin panel.</p>
+    <main className="h-dvh bg-gray-1 text-gray-12 flex flex-col gap-12 items-center justify-center">
+      <div className="space-y-6 text-center">
+        <h1 className="text-3xl font-semibold">Nacer Codes</h1>
+        <p className="text-gray-10">This is the admin panel.</p>
+      </div>
     </main>
   )
 }
